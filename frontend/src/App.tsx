1d486259615a0dd90a95bbf8d9c4fe93c4eeedd2
Vedant <vedantmohol18@gmail.com>
2025-06-27 23:46:08 +0530

    Installed React JS and Chakra UI

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading, Text } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box p={8}>
+      <Heading>Welcome to FinGlance</Heading>
+      <Text mt={4}>Start building your dashboard here.</Text>
+    </Box>
+  );
+}
+
+export default App;
