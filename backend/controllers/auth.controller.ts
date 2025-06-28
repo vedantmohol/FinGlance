db7c7beaa00c350c02bd8aca21d17bfcc26299e5
Vedant <vedantmohol18@gmail.com>
2025-06-28 14:21:24 +0530

    Created login user API route

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import User from '../models/user.model';
+import jwt from 'jsonwebtoken';
 
 export const registerUser = async (req: Request, res: Response) => {
   try {
@@ -17,5 +18,40 @@ export const registerUser = async (req: Request, res: Response) => {
     return res.status(201).json({ message: 'User registered successfully' });
   } catch (err) {
     return res.status(500).json({ message: 'Registration failed', error: err });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Email and password are required' });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: 'User does not exists!' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET!
+    );
+
+    const { password: _, ...userInfo } = user.toObject();
+
+    res.status(200).json({
+      message: 'Login successful',
+      token,
+      user: userInfo,
+    });
+  } catch (err) {
+    res.status(500).json({ message: 'Login failed', error: err });
   }
 };
--- backend/routes/auth.route.ts
+++ backend/routes/auth.route.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { registerUser } from '../controllers/auth.controller';
+import { loginUser, registerUser } from '../controllers/auth.controller';
 
 const router = express.Router();
 
 router.post('/register', (req, res) => {registerUser(req, res)});
+router.post('/login', (req, res) => {loginUser(req, res)});
 
 export default router;
